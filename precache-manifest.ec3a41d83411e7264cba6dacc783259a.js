de606e2b917cdb948e3ba0d30bbb5e8316c5e473
Lee Raybone <lee.raybone@cloudbooking.com>
2020-04-22 09:57:14 +0100

    Updates

---
+++ precache-manifest.ec3a41d83411e7264cba6dacc783259a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aeaf035b3b4a50761f524e7a3e2b573",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0449506fa1dc03bb6124",
+    "url": "/monsters-rolodex/static/css/main.e93bc6f7.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0449506fa1dc03bb6124",
+    "url": "/monsters-rolodex/static/js/main.87037f87.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
